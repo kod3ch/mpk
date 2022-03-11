069d7e2eed2e627f26ea2856aaed473c50527e62
Vladyslav Artemenko <artemenko.wlad@gmail.com>
2022-03-11 23:30:59 +0100

    new icon

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="travel_bus.ico">
+  <link rel="shortcut icon" href="bus.ico">
   <link rel="stylesheet" href="main.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -19,7 +19,7 @@
   <nav class="navbar navbar-dark shadow vw-100" style="background-color: #2032b3;">
     <div class="container-sm">
       <a class="navbar-brand" href="#">
-        <img src="travel_bus.ico" alt="" width="30" height="24" class="d-inline-block align-text-top">
+        <img src="bus.ico" alt="" width="30" height="24" class="d-inline-block align-text-top">
         &nbsp;MPK | System biletowy
       </a>
     </div>
